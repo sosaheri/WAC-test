ecf0f9403bccb1c148bcc5b8cd841f316ced9007
sosaheri <sosaheriberto2001@gmail.com>
2021-05-28 16:45:56 -0400

    setup repo

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yiitestdb',
+    'username' => 'yiitest_user',
+    'password' => 'XtByqo%asj81CJmM[dfC',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
